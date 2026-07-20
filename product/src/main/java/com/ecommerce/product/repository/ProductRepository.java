5ca3d453f6add2b46627fdba27a7ad2ebef9b1b9
xoUtkarsh <trivediutkarsh20@gmail.com>
2026-07-20 23:42:49 +0530

    Product Service

---
+++ product/src/main/java/com/ecommerce/product/repository/ProductRepository.java
@@ -0,0 +1,17 @@
+package com.ecommerce.product.repository;
+
+import com.ecommerce.product.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findByActiveTrue();
+
+    @Query("SELECT p FROM products p WHERE p.active = true AND p.stockQuantity > 0 AND LOWER(p.name) LIKE LOWER(CONCAT('%', :keyword, '%'))")
+    List<Product> searchProducts(@Param("keyword") String keyword);
+}
