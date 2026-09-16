2144b6844e223ffeab25ec844fb17db14cbc2bf8
xoUtkarsh <trivediutkarsh20@gmail.com>
2026-09-16 14:39:51 +0530

    Enabling distrubted tracing using zipkin and grafana

--- order/src/main/java/com/ecommerce/order/clients/RestClientConfig.java
+++ order/src/main/java/com/ecommerce/order/clients/RestClientConfig.java
@@ -1,22 +1,60 @@
 package com.ecommerce.order.clients;
 
+import io.micrometer.observation.ObservationRegistry;
+import io.micrometer.tracing.Tracer;
+import io.micrometer.tracing.propagation.Propagator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.http.client.ClientHttpRequestInterceptor;
 import org.springframework.web.client.RestClient;
 
 @Configuration
 public class RestClientConfig {
+
+    @Autowired(required = false)
+    private ObservationRegistry observationRegistry;
+
+    @Autowired(required = false)
+    private Tracer tracer;
+
+    @Autowired(required = false)
+    private Propagator propagator;
+
     @Bean
     @Primary
     public RestClient.Builder restClientBuilder() {
-        return RestClient.builder();
+        return buildTracedClient();
     }
 
     @Bean
     @LoadBalanced
     public RestClient.Builder loadBalancedRestClientBuilder() {
-        return RestClient.builder();
+        return buildTracedClient();
+    }
+
+    private RestClient.Builder buildTracedClient() {
+        RestClient.Builder builder = RestClient.builder();
+
+        if (observationRegistry != null) {
+            builder.requestInterceptor(createTracingInterceptor());
+        }
+
+        return builder;
+    }
+
+    private ClientHttpRequestInterceptor createTracingInterceptor() {
+        return (request, body, execution) -> {
+            if (tracer != null && propagator != null && tracer.currentSpan() != null) {
+                propagator.inject(
+                        tracer.currentTraceContext().context(),
+                        request.getHeaders(),
+                        (carrier, key, value) -> carrier.add(key, value)
+                );
+            }
+            return execution.execute(request, body);
+        };
     }
 }
